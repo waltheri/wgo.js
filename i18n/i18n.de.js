c6c8b4b6eab9c18a328922fb4914ae267b7f357e
ichderfisch <ichderfisch@users.noreply.github.com>
2014-05-09 19:56:12 +0200

    Create i18n.de.js

---
+++ i18n/i18n.de.js
@@ -0,0 +1,62 @@
+// This file serves as template for translations
+// You can just replace english strings with translated strings.
+
+WGo.lang = "en"; // set language code
+
+WGo.i18n[WGo.lang] = {
+	"about-text": "<h1>WGo.js Player 2.0</h1>"
+				+ "<p>WGo.js Player is extension of WGo.js, HTML5 library for purposes of game of go. It allows to replay go game records and it has many features like score counting. It is also designed to be easily extendable.</p>"
+				+ "<p>WGo.js is open source licensed under <a href='http://en.wikipedia.org/wiki/MIT_License' target='_blank'>MIT license</a>. You can use and modify any code from this project.</p>"
+				+ "<p>You can find more information at <a href='http://wgo.waltheri.net/player' target='_blank'>wgo.waltheri.net/player</a></p>"
+				+ "<p>Copyright &copy; 2013 Jan Prokop</p>",
+	"black": "Black",
+	"white": "White",
+	"DT": "Date",
+	"KM": "Komi",
+	"HA": "Handicap",
+	"AN": "Annotations",
+	"CP": "Copyright",
+	"GC": "Game comments",
+	"GN": "Game name",
+	"ON": "Fuseki",
+	"OT": "Overtime",
+	"TM": "Basic time",
+	"RE": "Result",
+	"RO": "Round",
+	"RU": "Rules",
+	"US": "Recorder",
+	"PC": "Place",
+	"EV": "Event",
+	"SO": "Source",
+	"none": "none",
+	"minute": "minute",
+	"minutes": "minutes",
+	"second": "second",
+	"seconds": "seconds",
+	"bpass": "Black passed.",
+	"wpass": "White passed.",
+	"show": "show",
+	"res-show-tip": "Click to show result.",
+	"BP:closemsg": "click anywhere to close this window",
+	"rank": "Rank",
+	"caps": "Caps",
+	"time": "Time",
+	"about": "About",
+	"first": "First",
+	"multiprev": "10 moves back",
+	"previous": "Previous",
+	"next": "Next",
+	"multinext": "10 moves forward",
+	"last": "Last",
+	"switch-coo": "Display coordinates",
+	"menu": "Menu",
+	"comments": "Comments",
+	"gameinfo": "Game info",
+	"editmode": "Edit mode",
+	"permalink": "Permanent link",
+	"scoremode": "Count score",
+	"score": "Score",
+	"bwin": "Black wins by $ points.",
+	"wwin": "White wins by $ points.",
+	"help_score": "Click on stones to mark them dead or alive. You can also set and unset territory points by clicking on them. Territories must be completely bordered.",
+}
