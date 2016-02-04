0c7b2c2360b2bb0ca8b750faf6c394a821e9b819
Sebastian Heuchler <nerai@users.noreply.github.com>
2016-02-04 01:41:32 +0100

    improve language

--- i18n/i18n.en.js
+++ i18n/i18n.en.js
@@ -5,7 +5,7 @@ WGo.lang = "en"; // set language code
 
 WGo.i18n[WGo.lang] = {
 	"about-text": "<h1>WGo.js Player 2.0</h1>"
-				+ "<p>WGo.js Player is extension of WGo.js, HTML5 library for purposes of game of go. It allows to replay go game records and it has many features like score counting. It is also designed to be easily extendable.</p>"
+				+ "<p>'WGo.js Player' is an extension of WGo.js, a HTML5 library for purposes of game of go. It allows replaying go game records and offers many features, e.g. score counting. It is also designed to be easily extendable.</p>"
 				+ "<p>WGo.js is open source licensed under <a href='http://en.wikipedia.org/wiki/MIT_License' target='_blank'>MIT license</a>. You can use and modify any code from this project.</p>"
 				+ "<p>You can find more information at <a href='http://wgo.waltheri.net/player' target='_blank'>wgo.waltheri.net/player</a></p>"
 				+ "<p>Copyright &copy; 2013 Jan Prokop</p>",
