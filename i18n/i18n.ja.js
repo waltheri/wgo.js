ee55b99b3bff28145c71a9e898e810531e8030a7
Kenji ITO <mystralkk@gmail.com>
2016-08-14 13:34:46 +0900

    Added Japanese language file

---
+++ i18n/i18n.ja.js
@@ -0,0 +1,62 @@
+// This file serves as template for translations
+// You can just replace english strings with translated strings.
+
+WGo.lang = "ja"; // set language code
+
+WGo.i18n[WGo.lang] = {
+	"about-text": "<h1>WGo.js Player 2.0</h1>"
+				+ "<p>'WGo.js Player' is an extension of WGo.js, a HTML5 library for purposes of game of go. It allows replaying go game records and offers many features, e.g. score counting. It is also designed to be easily extendable.</p>"
+				+ "<p>WGo.js is open source licensed under <a href='http://en.wikipedia.org/wiki/MIT_License' target='_blank'>MIT license</a>. You can use and modify any code from this project.</p>"
+				+ "<p>You can find more information at <a href='http://wgo.waltheri.net/player' target='_blank'>wgo.waltheri.net/player</a></p>"
+				+ "<p>Copyright &copy; 2013 Jan Prokop</p>",
+	"black": "黒",
+	"white": "白",
+	"DT": "日付",
+	"KM": "コミ",
+	"HA": "ハンディ",
+	"AN": "注釈",
+	"CP": "Copyright",
+	"GC": "対局のコメント",
+	"GN": "対局名",
+	"ON": "布石",
+	"OT": "時間切れ",
+	"TM": "基本持ち時間",
+	"RE": "結果",
+	"RO": "局",
+	"RU": "ルール",
+	"US": "記録",
+	"PC": "場所",
+	"EV": "イベント",
+	"SO": "ソース",
+	"none": "なし",
+	"minute": "分",
+	"minutes": "分",
+	"second": "秒",
+	"seconds": "秒",
+	"bpass": "黒番パス",
+	"wpass": "白番パス",
+	"show": "表示",
+	"res-show-tip": "結果を表示するにはクリック",
+	"BP:closemsg": "このウィンドウを閉じるにはクリックしてください",
+	"rank": "ランク",
+	"caps": "ハンディ",
+	"time": "時間",
+	"about": "About",
+	"first": "最初",
+	"multiprev": "10手戻る",
+	"previous": "戻る",
+	"next": "進む",
+	"multinext": "10手進む",
+	"last": "最後",
+	"switch-coo": "座標を表示",
+	"menu": "メニュー",
+	"comments": "コメント",
+	"gameinfo": "対局情報",
+	"editmode": "編集モード",
+	"permalink": "永続リンク",
+	"scoremode": "目算する",
+	"score": "目数",
+	"bwin": "黒の $ 目勝ち",
+	"wwin": "白の $ 目勝ち",
+	"help_score": "石の生死をマークするにはクリックしてください。You can also set and unset territory points by clicking on them. Territories must be completely bordered.",
+};
