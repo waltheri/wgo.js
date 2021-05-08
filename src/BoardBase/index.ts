4ad03c31764b623d68fad35a14a1cd086c5e4a3d
Jan Prokop <jprokop4@gmail.com>
2021-05-08 15:47:42 +0200

    Version increase.

--- src/BoardBase/index.ts
+++ src/BoardBase/index.ts
@@ -1,7 +1,7 @@
 export { default as BoardObject } from './BoardObject';
 export { default as FieldBoardObject } from './FieldBoardObject';
-export { default as LabelBoardObject } from './LabelBoardObject';
 export { default as MarkupBoardObject } from './MarkupBoardObject';
+export { default as LabelBoardObject } from './LabelBoardObject';
 export { default as LineBoardObject } from './LineBoardObject';
 export { default as BoardBase } from './BoardBase';
 export { Board, BoardViewport } from './types';
